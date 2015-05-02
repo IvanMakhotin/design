e34010cc1b14d26a7f7f3b3572abffd27931c471
Ivan Makhotin <i.makhotin@yandex.ru>
2015-05-02 12:24:38 +0500

    1. Fluent API 2. using

--- PerfLogger/Program.cs
+++ PerfLogger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 
 namespace PerfLogger
@@ -7,14 +8,39 @@ namespace PerfLogger
 	{
 		static void Main(string[] args)
 		{
-			/*
 			var sum = 0.0;
 			using (PerfLogger.Measure(t => Console.WriteLine("for: {0}", t)))
 				for (var i = 0; i < 100000000; i++) sum += i;
 			using (PerfLogger.Measure(t => Console.WriteLine("linq: {0}", t)))
 				sum -= Enumerable.Range(0, 100000000).Sum(i => (double)i);
 			Console.WriteLine(sum);
-			*/
+			
 		}
+
+
+	    class PerfLogger : IDisposable
+	    {
+	        private  Stopwatch timer;
+	        private Action<TimeSpan> logAction;
+
+	        public PerfLogger()
+	        {
+	            timer = new Stopwatch();
+	        }
+	        public static PerfLogger Measure(Action<TimeSpan> logActionFunction)
+	        {
+	            PerfLogger logger = new PerfLogger();
+	            logger.logAction = logActionFunction;
+                logger.timer.Start();
+	            return logger;
+
+	        }
+
+	        public void Dispose()
+	        {
+	            timer.Stop();
+                logAction.Invoke(timer.Elapsed);
+	        }
+	    }
 	}
 }
